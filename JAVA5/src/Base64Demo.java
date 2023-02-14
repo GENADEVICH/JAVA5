c1a368ad5781247803931799832fbc30033fbff6
GENADEVICH <116941467+GENADEVICH@users.noreply.github.com>
2023-02-14 09:47:30 +0300

    С1

---
+++ JAVA5/src/Base64Demo.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import sun.misc.*;
+public class Base64Demo {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("usage: Base64Demo text");
+            System.exit(1);
+        }
+        String text = args [0];
+        BASE64Encoder enc = new BASE64Encoder();
+        String encoded = enc.encode(text.getBytes());
+        enc = null;
+
+        String decoded = null;
+        try {
+            BASE64Decoder dec = new BASE64Decoder();
+            decoded = new String(dec.decodeBuffer(encoded));
+            dec = null;
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        System.out.println("Закодировано: " + encoded);
+        System.out.println("Декадировано: " + encoded);
+        encoded = null;
+        decoded = null;
+    }
+
+}
---
+++ JAVA5/src/Stack.java
@@ -0,0 +1,42 @@
+public class Stack {
+    private Object[] theArray;
+    private int topOfStack;
+    public Stack(){
+        theArray = new Object[DEFAULT_CAPACITY];
+        topOfStack = -1;
+    }
+    boolean isEmpty(){
+        return topOfStack == -1;
+    }
+    Object TOP(){
+        if(isEmpty())
+            return null;
+        return theArray[topOfStack];
+    }
+    void pop(){
+        if(isEmpty())
+            return;
+        topOfStack--;
+    }
+    Object topAndPop(){
+        if(isEmpty())
+            return null;
+        return theArray[topOfStack--];
+    }
+    void push(Object x){
+        if(topOfStack + 1 == theArray.length)
+            doubleArray();
+        theArray[ ++topOfStack ] = x;
+    }
+    void makeEmpty(){
+        topOfStack = -1;
+    }
+    private  void doubleArray(){
+        Object [ ] newArray;
+        newArray = new Object[ theArray.length * 2];
+        for( int i = 0; i <theArray.length; i++){
+            System.arraycopy(theArray, 0, newArray, 0, theArray.length);
+        }
+        theArray = newArray;
+    }
+}
